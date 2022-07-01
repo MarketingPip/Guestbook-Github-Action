c422005428372bb5f004d011d104b00727c73138
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-01 04:45:40 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
-      && git add -A && git commit -m '$*' --allow-empty \
+      && git add -A && git commit -m "Updated Static Content" --allow-empty \
       && git push -u origin HEAD"
