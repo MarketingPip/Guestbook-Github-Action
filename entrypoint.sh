b9811205fa167e0120da33f299f4b7d5e2751a6e
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-01 04:51:22 -0400

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-sh -c "git config--global user.name "github-actions[bot]"' \
-      && git config  --global user.email "41898282+github-actions[bot]@users.noreply.github.com"\
-      && git add -A && git commit -m "Updated Static Content" --allow-empty \
-      && git push -u origin HEAD"
